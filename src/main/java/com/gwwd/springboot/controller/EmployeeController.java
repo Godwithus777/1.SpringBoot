f05de7be8d1a41294889b19bf927834fd2fe1c7b
username <17801190934@163.com>
2020-09-09 13:32:31 +0800

    6.1CRUD的修改和添加

--- src/main/java/com/gwwd/springboot/controller/EmployeeController.java
+++ src/main/java/com/gwwd/springboot/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -63,14 +64,25 @@ public class EmployeeController {
         return "redirect:/emps";
     }
 
+    //来到修改页面，查出当前员工，在页面回显
     @GetMapping("/emp/{id}")
-    public String editPage(@PathVariable("id") Integer id, Model model) {
+    public String toEditPage(@PathVariable("id") Integer id,Model model){
         Employee employee = employeeDao.get(id);
         model.addAttribute("emp",employee);
 
+        //页面要显示所有的部门列表
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("depts",departments);
-
+        //回到修改页面(add是一个修改添加二合一的页面);
         return "emp/add";
     }
+
+    //员工修改；需要提交员工id；
+    @PutMapping("/emp")
+    public String updateEmployee(Employee employee){
+        System.out.println("修改的员工数据："+employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
 }

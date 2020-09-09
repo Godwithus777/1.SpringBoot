ae68a12773333662a91cc93adf4f3c9a1396479d
username <17801190934@163.com>
2020-09-09 13:28:37 +0800

    6.添加add.html错误页面

--- src/main/java/com/gwwd/springboot/controller/EmployeeController.java
+++ src/main/java/com/gwwd/springboot/controller/EmployeeController.java
@@ -1,12 +1,16 @@
 package com.gwwd.springboot.controller;
 
 
+import com.gwwd.springboot.dao.DepartmentDao;
 import com.gwwd.springboot.dao.EmployeeDao;
+import com.gwwd.springboot.entities.Department;
 import com.gwwd.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -15,6 +19,9 @@ public class EmployeeController {
 
     @Autowired
     EmployeeDao employeeDao;
+
+    @Autowired
+    DepartmentDao departmentDao;
 
 
     // 查询所有员工返回列表页面
@@ -31,4 +38,39 @@ public class EmployeeController {
 
     }
 
+    // 添加员工
+    @GetMapping("/emp")
+    public String toAddPage(Model model) {
+
+        // 来到添加页面,查出所有部门,在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+
+        return "emp/add";
+    }
+
+    //员工添加
+    //SpringMVC自动将请求参数和入参对象的属性进行一一绑定；要求请求参数的名字和javaBean入参的对象里面的属性名是一样的
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        //来到员工列表页面
+
+        System.out.println("保存的员工信息："+employee);
+        //保存员工
+        employeeDao.save(employee);
+        // redirect: 表示重定向到一个地址  /代表当前项目路径
+        // forward: 表示转发到一个地址
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String editPage(@PathVariable("id") Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+
+        return "emp/add";
+    }
 }
